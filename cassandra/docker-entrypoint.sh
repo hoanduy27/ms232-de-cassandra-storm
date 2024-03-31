a1f1d970958a7c55ba364bbec005074dd66292c0
hoanduy27 <hoanduy27@gmail.com>
2024-04-01 00:14:26 +0700

    update: automatically load raw data to cassandra

--- cassandra/docker-entrypoint.sh
+++ cassandra/docker-entrypoint.sh
@@ -1,94 +1,52 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+## 
+## This script will generate a patched docker-entrypoint.sh that:
+## - executes any *.sh script found in /docker-entrypoint-initdb.d
+## - boots cassandra up
+## - executes any *.cql script found in docker-entrypoint-initdb.d
+##
+## It is compatible with any cassandra:* image
+##
 
-# first arg is `-f` or `--some-option`
-# or there are no args
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-	set -- cassandra -f "$@"
+
+## Create script that executes files found in docker-entrypoint-initdb.d/
+
+cat <<'EOF' >> /run-init-scripts.sh
+#!/usr/bin/env bash
+LOCK=/var/lib/cassandra/_init.done
+INIT_DIR=docker-entrypoint-initdb.d
+if [ -f "$LOCK" ]; then
+    echo "@@ Initialization already performed."
+    exit 0
 fi
+cd $INIT_DIR
+echo "@@ Executing bash scripts found in $INIT_DIR"
+# execute scripts found in INIT_DIR
 
-# allow the container to be started with `--user`
-if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
-	find "$CASSANDRA_CONF" /var/lib/cassandra /var/log/cassandra \
-		\! -user cassandra -exec chown cassandra '{}' +
-	exec gosu cassandra "$BASH_SOURCE" "$@"
-fi
+# wait for cassandra to be ready and execute cql in background
+(
+    while ! cqlsh -e 'describe cluster' > /dev/null 2>&1; do sleep 6; done
+    echo "$0: Cassandra cluster ready: executing cql scripts found in $INIT_DIR"
+    cqlsh -f /src/schema.cql
+    python /src/main.py
+    
+    # mark things as initialized (in case /var/lib/cassandra was mapped to a local folder)
+    touch $LOCK
+) &
+EOF
 
-_ip_address() {
-	# scrape the first non-localhost IP address of the container
-	# in Swarm Mode, we often get two IPs -- the container IP, and the (shared) VIP, and the container IP should always be first
-	ip address | awk '
-		$1 != "inet" { next } # only lines with ip addresses
-		$NF == "lo" { next } # skip loopback devices
-		$2 ~ /^127[.]/ { next } # skip loopback addresses
-		$2 ~ /^169[.]254[.]/ { next } # skip link-local addresses
-		{
-			gsub(/\/.+$/, "", $2)
-			print $2
-			exit
-		}
-	'
-}
-
-# "sed -i", but without "mv" (which doesn't work on a bind-mounted file, for example)
-_sed-in-place() {
-	local filename="$1"; shift
-	local tempFile
-	tempFile="$(mktemp)"
-	sed "$@" "$filename" > "$tempFile"
-	cat "$tempFile" > "$filename"
-	rm "$tempFile"
-}
-
-if [ "$1" = 'cassandra' ]; then
-	: ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
-
-	: ${CASSANDRA_LISTEN_ADDRESS='auto'}
-	if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
-		CASSANDRA_LISTEN_ADDRESS="$(_ip_address)"
-	fi
-
-	: ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
-
-	if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
-		CASSANDRA_BROADCAST_ADDRESS="$(_ip_address)"
-	fi
-	: ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
-
-	if [ -n "${CASSANDRA_NAME:+1}" ]; then
-		: ${CASSANDRA_SEEDS:="cassandra"}
-	fi
-	: ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
-
-	_sed-in-place "$CASSANDRA_CONF/cassandra.yaml" \
-		-r 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/'
-
-	for yaml in \
-		broadcast_address \
-		broadcast_rpc_address \
-		cluster_name \
-		endpoint_snitch \
-		listen_address \
-		num_tokens \
-		rpc_address \
-		start_rpc \
-	; do
-		var="CASSANDRA_${yaml^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			_sed-in-place "$CASSANDRA_CONF/cassandra.yaml" \
-				-r 's/^(# )?('"$yaml"':).*/\2 '"$val"'/'
-		fi
-	done
-
-	for rackdc in dc rack; do
-		var="CASSANDRA_${rackdc^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			_sed-in-place "$CASSANDRA_CONF/cassandra-rackdc.properties" \
-				-r 's/^('"$rackdc"'=).*/\1 '"$val"'/'
-		fi
-	done
-fi
-
+## Patch existing entrypoint to call our script in the background
+# This has been inspired by https://www.thetopsites.net/article/51594713.shtml
+EP=/patched-entrypoint.sh
+sed '$ d' /docker-entrypoint.sh > $EP
+cat <<'EOF' >> $EP
+/run-init-scripts.sh &
 exec "$@"
+EOF
+
+# Make both scripts executable
+chmod +x /run-init-scripts.sh
+chmod +x $EP
+
+# Call the new entrypoint
+$EP "$@"
